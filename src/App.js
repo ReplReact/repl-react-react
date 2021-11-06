c73e45d36f8f7a0ce31de5806292a25807b34efc
Ian Sears <ian.shane.sears@gmail.com>
2021-11-06 09:07:51 -0700

    added theme and font size selector for editor component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './assets/util.css';
 import Editor from './components/Editor-React-Ace/Editor-React-Ace'
 import 'semantic-ui-css/semantic.min.css'
 import { Container, Header } from 'semantic-ui-react'
@@ -8,11 +8,9 @@ import { Container, Header } from 'semantic-ui-react'
 function App() {
 
   return (
-    <div className="App">
-      <Container fluid>
+      <Container className="App" fluid>
         <Editor />
       </Container>
-    </div>
   );
 }
 
